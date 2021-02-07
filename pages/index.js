6c7041271d36995082e9c29bf5a8dd949ff470a3
Carlo Bruno <carlofbruno@gmail.com>
2021-02-07 14:28:50 -0800

    create data fetching method in index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import Head from 'next/head';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=6'
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+
+export default function Home({ articles }) {
+  console.log(articles);
+  const titleList = articles.map((article) => {
+    return <h3 key={article.id}>{article.title}</h3>;
+  });
+
   return (
     <div>
       <Head>
@@ -10,7 +28,7 @@ export default function Home() {
           content='web development, programming'
         />
       </Head>
-      <h1>Next.js Blog ⚛︎🔥</h1>
+      {titleList}
     </div>
   );
 }
